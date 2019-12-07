b03aceff048410fe709ffef8ec86512a1b2e74a8
Vali Shah <vali.shah7@gmail.com>
2019-12-07 23:47:06 +0000

    Modified the references to release notifier

--- dist/index.js
+++ dist/index.js
@@ -3033,21 +3033,17 @@ const fetchReleases = async (request) => {
 
 const processAction = async () => {
     try {
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        const token = core.getInput('repo-token', {required: true});
-        console.log(`Repo token ${token}`);
-        console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
+        const token = core.getInput('repo-token');
+        /* const time = (new Date()).toTimeString();
         core.setOutput("time", time);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        // const payload = JSON.stringify(github.context.payload, undefined, 2)
-        // console.log(`The event payload: ${payload}`);
+        Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`); */
         const repository = github.context.payload.repository;
         const repository_name = repository.name;
         const owner_name =  repository.owner.name;
-        console.log(`Repository: ${repository_name}`);
-        console.log(`Owner ${owner_name}`);
+       /*  console.log(`Repository: ${repository_name}`);
+        console.log(`Owner ${owner_name}`); */
         const releases = await fetchReleases({
             token,
             repository_name,
